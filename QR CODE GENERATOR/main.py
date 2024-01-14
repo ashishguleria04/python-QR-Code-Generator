3edff66d66e8dae902a5079ce088fe8a0d999111
Ashish Guleria <110624199+ashishguleria04@users.noreply.github.com>
2024-01-14 21:32:51 +0530

    Update main.py

--- QR CODE GENERATOR/main.py
+++ QR CODE GENERATOR/main.py
@@ -2,7 +2,7 @@ import qrcode
 
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=50,border=1)
 
-qr.add_data("https://www.youtube.com/watch?v=l4ugfcj7qrI")
+qr.add_data("<your link/text/anything you want to convert into qr code here>")
 
 qr.make(fit=True)
 
